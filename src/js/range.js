cb7e88961223d742f44d17b7b98798ec95a6f0c6
Erling Owe <owe@vecora.no>
2018-07-09 01:21:25 +0200

    Should now work with multiple range elements

--- src/js/range.js
+++ src/js/range.js
@@ -1,1 +1,7 @@
+document.documentElement.classList.add('js');
 
+document.querySelectorAll('[type=range]').forEach(function(range) {
+	range.addEventListener('input', function (e) {
+		range.style.setProperty('--val', +range.value);
+	}, false);
+});
