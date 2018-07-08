8d8149e1f0545b437a4ea0c70bcc75d3e9f551f6
Erling Owe <owe@vecora.no>
2018-07-08 19:50:39 +0200

    Rewrite

--- assets/main.js
+++ assets/main.js
@@ -1,23 +1,30 @@
+(function() {
 
-var cookieMessageToggle = document.getElementById("cookieMessageToggle");
+  var cookieMessageToggle = document.getElementById("cookieMessageToggle");
 
-function doCoockieMessageToggleText(cookieMessageToggle, cookieMessageStatus) {
-  if (cookieMessageStatus === 'accepted') {
-    cookieMessageToggle.innerHTML = "Activate cookie message";
-  } else if (typeof cookieMessageStatus === "undefined") {
-    cookieMessageToggle.innerHTML = "Deactivate cookie message";
-  }
-}
-
-cookieMessageToggle.addEventListener("click", function() {
-
-  if (localStorage.cookieMessageStatus === "accepted") {
-    delete localStorage.cookieMessageStatus;
-  } else if (typeof localStorage.cookieMessageStatus === "undefined") {
-    localStorage.cookieMessageStatus = "accepted";
+  function updateCoockieMessageToggleText() {
+    if (CookieMessage.accepted) {
+      cookieMessageToggle.innerHTML = "Activate cookie message";
+    } else {
+      cookieMessageToggle.innerHTML = "Deactivate cookie message";
+    }
   }
 
-  doCookieMessageVisibility();
-  doCoockieMessageToggleText(cookieMessageToggle, localStorage.cookieMessageStatus);
 
-});
+  // Initialize
+
+  updateCoockieMessageToggleText();
+
+  cookieMessageToggle.addEventListener("click", function() {
+
+    if (CookieMessage.accepted) {
+      CookieMessage.accepted = false;
+    } else {
+      CookieMessage.accepted = true;
+    }
+
+    updateCoockieMessageToggleText();
+
+  });
+
+})();
--- src/js/cookie-message.js
+++ src/js/cookie-message.js
@@ -1,16 +1,43 @@
-
-function doCookieMessageVisibility() {
-  if (localStorage.cookieMessageStatus === "accepted") {
-    document.querySelector(".cookie-message").style.display = "none";
-  } else if (typeof localStorage.cookieMessageStatus === "undefined") {
-    document.querySelector(".cookie-message").style.display = "block";
+CookieMessage = {
+  updateVisibility: function() {
+    if (CookieMessage.accepted) {
+      document.querySelector(".cookie-message").style.display = "none";
+    } else {
+      document.querySelector(".cookie-message").style.display = "block";
+    }
+  },
+  onStatusChange: function() {}, // Used by the docs
+  get exists() {
+    if (document.querySelector(".cookie-message")) {
+      return true;
+    } else {
+      return false;
+    }
+  },
+  get accepted() {
+    if (localStorage.cookieMessageStatus === "accepted") {
+      return true;
+    } else if (typeof localStorage.cookieMessageStatus === "undefined") {
+      return false;
+    }
+  },
+  set accepted(bool) {
+    if (bool) {
+      localStorage.cookieMessageStatus = "accepted";
+    } else {
+      delete localStorage.cookieMessageStatus;
+    }
+    CookieMessage.updateVisibility();
   }
-}
+};
 
 // Initialize
-doCookieMessageVisibility();
+if (CookieMessage.exists) {
 
-document.querySelector(".cookie-message button").addEventListener("click", function() {
-  localStorage.cookieMessageStatus = "accepted";
-  doCookieMessageVisibility();
-});
+  CookieMessage.updateVisibility();
+
+  document.querySelector(".cookie-message button").addEventListener("click", function() {
+    CookieMessage.accepted = true;
+  });
+
+}
