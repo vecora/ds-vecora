1dc0d03f466231276501db0deea471ef365cde97
Erling Owe <owe@vecora.no>
2018-07-08 18:14:34 +0200

    Hopefully fixed logic

--- assets/main.js
+++ assets/main.js
@@ -1,14 +1,26 @@
 
 var cookieMessageToggle = document.getElementById("cookieMessageToggle")
 
-cookieMessageToggle.addEventListener("click", function() {
-  if (localStorage.cookieMessage) {
-    document.querySelector(".cookie-message").style.display = "none";
-    cookieMessageToggle.innerHTML = "Deactivate cookie message";
-    localStorage.cookieMessage = "accepted";
-  } else {
-    document.querySelector(".cookie-message").style.display = "block";
+function setCoockieMessageToggleText(cookieMessageToggle, cookieMessageStatus) {
+
+  if (cookieMessageStatus === 'accepted') {
     cookieMessageToggle.innerHTML = "Activate cookie message";
-    localStorage.cookieMessage = undefined;
+  } else if (cookieMessageStatus === undefined) {
+    cookieMessageToggle.innerHTML = "Deactivate cookie message";
   }
+
+}
+
+cookieMessageToggle.addEventListener("click", function() {
+
+  if (localStorage.cookieMessageStatus === "accepted") {
+    document.querySelector(".cookie-message").style.display = "none";
+    localStorage.cookieMessageStatus = undefined;
+  } else if (localStorage.cookieMessageStatus === undefined) {
+    document.querySelector(".cookie-message").style.display = "block";
+    localStorage.cookieMessageStatus = "accepted";
+  }
+
+  setCoockieMessageToggleText(cookieMessageToggle, localStorage.cookieMessageStatus);
+
 });
--- src/js/cookie-message.js
+++ src/js/cookie-message.js
@@ -1,9 +1,9 @@
 
-if (!localStorage.cookieMessage) {
+if (!localStorage.cookieMessageStatus) {
   document.querySelector(".cookie-message").style.display = "block";
 }
 
 document.querySelector(".cookie-message button").addEventListener("click", function() {
   document.querySelector(".cookie-message").style.display = "none";
-  localStorage.cookieMessage = "accepted";
+  localStorage.cookieMessageStatus = "accepted";
 });
