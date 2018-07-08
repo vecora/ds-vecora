c5ff7235a9ae0d74091d071116fc454fe6d6e917
Erling Owe <owe@vecora.no>
2018-07-09 01:16:42 +0200

    Test

--- src/js/range.js
+++ src/js/range.js
@@ -1,7 +1,1 @@
-var _R = document.querySelector('[type=range]');
 
-document.documentElement.classList.add('js');
-
-_R.addEventListener('input', function (e) {
-	_R.style.setProperty('--val', +_R.value);
-}, false);
