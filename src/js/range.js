99cd795103b9c470bb386e13e1e381606f79ed9b
Erling Owe <owe@vecora.no>
2018-07-13 23:28:43 +0200

    Tweaked styles for range

--- src/js/range.js
+++ src/js/range.js
@@ -1,4 +1,4 @@
-document.documentElement.classList.add('js');
+// document.documentElement.classList.add('js');
 
 document.querySelectorAll('[type=range]').forEach(function(range) {
 	range.addEventListener('input', function (e) {
