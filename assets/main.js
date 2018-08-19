8c9063506d16e9e18bd955dc941f428a74dab7ef
Erling Owe <owe@vecora.no>
2018-08-19 23:34:08 +0200

    Don't run script unless the app is on the page

--- assets/main.js
+++ assets/main.js
@@ -38,33 +38,36 @@
 
 
 // Theme switcher
+if (document.querySelector("#theme-switcher")) {
 
-// Initialize
-var themeClass = localStorage.getItem('themeClass');
-document.documentElement.className = themeClass;
-$(document).ready(function() {
-  switch (themeClass) {
-    case "theme-dark":
-      document.getElementById('theme-dark').checked = true;
-      break;
-    case "theme-corporate":
-      document.getElementById('theme-corporate').checked = true;
-      break;
-    case "theme-sales":
-      document.getElementById('theme-sales').checked = true;
-      break;
-    default:
-      document.getElementById('theme-default').checked = true;
-  }
-});
-
-
-var themeSwitcherApp = new Vue({
-  el: '#theme-switcher',
-  methods: {
-    switchTheme: function (themeClass) {
-      document.documentElement.className = themeClass;
-      localStorage.setItem("themeClass", themeClass);
+  // Initialize
+  var themeClass = localStorage.getItem('themeClass');
+  document.documentElement.className = themeClass;
+  $(document).ready(function() {
+    switch (themeClass) {
+      case "theme-dark":
+        document.getElementById('theme-dark').checked = true;
+        break;
+      case "theme-corporate":
+        document.getElementById('theme-corporate').checked = true;
+        break;
+      case "theme-sales":
+        document.getElementById('theme-sales').checked = true;
+        break;
+      default:
+        document.getElementById('theme-default').checked = true;
     }
-  }
-})
+  });
+
+
+  var themeSwitcherApp = new Vue({
+    el: '#theme-switcher',
+    methods: {
+      switchTheme: function (themeClass) {
+        document.documentElement.className = themeClass;
+        localStorage.setItem("themeClass", themeClass);
+      }
+    }
+  });
+
+}
